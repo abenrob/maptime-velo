98addd32928c6155ae99856382ebd607b01dce12
abenrob <adamben.roberts@gmail.com>
2016-06-05 13:56:11 +0200

    actions for each route type

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
+// define mapbox token for map creation
 L.mapbox.accessToken = 'pk.eyJ1IjoiYWJlbnJvYiIsImEiOiJEYmh3WWNJIn0.fus8CLBKPBHDvSxiayhJyg';
+
+// set up map
 var map = L.mapbox.map('map', 'mapbox.streets')
     .setView([45.186502, 5.736339], 13);
+
+// define global variables
 var pos = [];
 var posMarker;
+var addressPos = [];
 var addressMarker;
 var bikeRouteMM;
 var bikeRouteII;
 
+// function to close mobile nav menu
 function closeNav() {
   $('.navHeaderCollapse').collapse('hide');
 };
 
+// function to clear routes from map
+function clearRoutes() {
+  if (bikeRouteMM) {map.removeLayer(bikeRouteMM);};
+
+  if (bikeRouteII) {map.removeLayer(bikeRouteII);};
+};
+
+// function for successful geolocaiton
 function geoSuccess(position) {
   pos = [position.coords.latitude, position.coords.longitude];
+
   map.panTo(pos);
   if (posMarker) {map.removeLayer(posMarker);};
 
@@ -23,38 +39,34 @@ function geoSuccess(position) {
       'marker-color': '#fa0',
     }),
   }).addTo(map);
-  $('#get_address').prop('disabled', false);
-  $('#address_search').prop('disabled', false);
-  closeNav();
 }
 
+// function for geolocation error
 function geoError(err) {
-  console.log(err, 'Sorry, no position available.');
+  alert(err, 'Sorry, no position available.');
 }
 
-var geoOptions = {
-  enableHighAccuracy: true,
-  maximumAge: 30000,
-  timeout: 27000,
-};
-
+// use html5 geolocation
 function getUserLocation() {
   if ('geolocation' in navigator) {
-    navigator.geolocation.getCurrentPosition(geoSuccess, geoError, geoOptions);
+    navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
   } else {
     console.log('No geolocation available');
   }
 }
 
-$('#get_position').click(function () {
-  getUserLocation();
-});
+// get address from BAN database https://adresse.data.gouv.fr/api/
+function getAddress() {
+  // remove any routes
+  clearRoutes();
 
-$('#get_address').click(function () {
+  // get value of search box
   var search = $('#address_search').val();
 
-  // https://adresse.data.gouv.fr/api/
+  // build uri for ban data
   var uri = 'https://api-adresse.data.gouv.fr/search/?q=' + search;
+
+  // if position set, use to inform address returns
   if (pos.length > 0) {
     uri = uri + '&lat=' + pos[0] + '&lon=' + pos[1];
   }
@@ -63,7 +75,7 @@ $('#get_address').click(function () {
     if (addressMarker) {map.removeLayer(addressMarker);};
 
     var coords = data.features[0].geometry.coordinates;
-    var addressPos = [coords[1], coords[0]];
+    addressPos = [coords[1], coords[0]];
 
     addressMarker = L.marker(addressPos, {
       icon: L.mapbox.marker.icon({
@@ -74,56 +86,95 @@ $('#get_address').click(function () {
     }).addTo(map);
     var group = new L.featureGroup([posMarker, addressMarker]);
     map.fitBounds(group.getBounds().pad(0.5));
-    closeNav();
-    getRoute(pos, addressPos);
-  });
-});
-
-function getMetromobiliteRoute(fromPos, toPos) {
-  var uri = 'http://data.metromobilite.fr/otp/routers/default/plan' +
-            '?mode=BICYCLE&fromPlace=' + fromPos + '&toPlace=' + toPos;
-  $.getJSON(uri, function (data) {
-    var pts = data.plan.itineraries[0].legs[0].legGeometry.points;
-    var decoded = polyline.decode(pts);
-    if (bikeRouteMM) {map.removeLayer(bikeRouteMM);};
-
-    bikeRouteMM = L.polyline(decoded, { color: 'blue' }).bindPopup('Metromobilité').addTo(map);
+    $('#route-menu').prop('disabled', false);
   });
 };
 
-function getItinisereRoute(fromPos, toPos) {
+function getMetromobiliteRoute(fromPos, toPos, callback) {
+  clearRoutes();
+
+  // build uri for API
+  var uri = 'http://data.metromobilite.fr/otp/routers/default/plan' +
+            '?mode=BICYCLE&fromPlace=' + fromPos + '&toPlace=' + toPos;
+
+  $.getJSON(uri, function (data) {
+    // get all node points from first returned trip
+    var pts = data.plan.itineraries[0].legs[0].legGeometry.points;
+
+    //use mapbox.polyline to decode encoded polyline string
+    var decoded = polyline.decode(pts);
+
+    // create polyline, assign color, bind popup, add to map
+    bikeRouteMM = L.polyline(decoded, { color: '#21881c' }).bindPopup('Metromobilité').addTo(map);
+
+    callback();
+  });
+};
+
+function getItinisereRoute(fromPos, toPos, algorithm, callback) {
+  clearRoutes();
+
+  // get date elements to fill in date/time requirement of API
   var d = new Date();
   var dateString = d.toISOString().slice(0, 10);
   var timeString = d.getHours() + '-' + (d.getMinutes() < 10 ? '0' : '') + d.getMinutes();
+
+  // build uri for API
   var uri = 'http://www.itinisere.fr/webServices/TransinfoService/api/journeyplanner/v2/' +
             'BikeTrip/json?DepLat=' + fromPos[0] + '&DepLon=' + fromPos[1] +
             '&ArrLat=' + toPos[0] + '&ArrLon=' + toPos[1] +
             '&Date=' + dateString + '&DepartureTime=' + timeString +
-            '&user_key=0016bf2ff47f630bab2e65bba954c091&Algorithm=FASTEST&callback=?';
+            '&user_key=0016bf2ff47f630bab2e65bba954c091&Algorithm=' + algorithm + '&callback=?';
+
   $.getJSON(uri, function (data) {
+    // get links for first trip
     var pathLinks = data.trips.Trip[0].sections.Section[0].Leg.pathLinks.PathLink;
 
+    // isolate geometry from WKT string
     var linkCoords = pathLinks.map(function (link) {
       return wellknown.parse(link.Geometry).coordinates;
     });
 
+    // combine all legs into one line
     var coords = linkCoords.reduce(function (a, b) {
       return a.concat(b);
     });
 
+    // swap long/lat to lat/log
     var swappedCoords = coords.map(function (pair) {
       return pair.reverse();
     });
 
-    bikeRouteII = L.polyline(swappedCoords, { color: 'red' }).bindPopup('ItinIsère').addTo(map);
+    // create polyline, assign color, bind popup, add to map
+    bikeRouteII = L.polyline(swappedCoords, { color: '#044571' }).bindPopup('ItinIsère').addTo(map);
+
+    callback();
   });
 };
 
-function getRoute(fromPos, toPos) {
-  if (bikeRouteMM) {map.removeLayer(bikeRouteMM);};
+// jQuery click actions
+// on get address click
+$('#get_address').click(function () {
+  getAddress();
+});
 
-  if (bikeRouteII) {map.removeLayer(bikeRouteII);};
+// on get location button click
+$('#get_position').click(function () {
+  // remove any routes
+  clearRoutes();
+  getUserLocation();
+});
 
-  getMetromobiliteRoute(fromPos, toPos);
-  getItinisereRoute(fromPos, toPos);
-}
+// on dropdown select, get the route
+$('#MM-route').click(function () {
+  getMetromobiliteRoute(pos, addressPos, function () {
+    closeNav();
+  });
+});
+
+// on dropdown select, get the route
+$('#II-route').click(function () {
+  getItinisereRoute(pos, addressPos, 'FASTEST', function () {
+    closeNav();
+  });
+});
